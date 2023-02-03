009b91aea4ce71b0e19228335427bbae045b584a
Ian Kersz <kerszamaral@gmail.com>
2023-02-03 11:16:51 -0300

    Added examples

---
+++ c/exemplos/teclado/teclado.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Entre com um numero inteiro: ");
+
+    int num;
+    scanf("%d", &num);
+    printf("Voce Digitou: %d", num);
+
+    return 0;
+}
---
+++ cpp/exemplos/teclado/teclado.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(void)
+{
+    std::cout << "Entre com um numero inteiro: ";
+    
+    int num;
+    std::cin >> num;
+    std::cout << "Voce digitou: " << num << std::endl;
+
+    return 0;
+}
